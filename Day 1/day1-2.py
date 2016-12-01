c835251a934c66cefe7a0e87147886a0f6d9c61a
M. Willis Monroe <willismonroe@gmail.com>
2016-12-01 13:44:10 -0800

    Reformatted code and added link to README.md

--- Day 1/day1-2.py
+++ Day 1/day1-2.py
@@ -5,15 +5,13 @@ with open('input.txt') as f:
     # ['L4', 'L3', 'R1' ...]
     data = f.read()
 
-start = [0,0]
-cur_loc = [0,0]
+start = [0, 0]
+cur_loc = [0, 0]
 facing = 0
 # dictionary for pretty printing our messages
 directions = {0: 'North', 90: 'East', 180: 'South', 270: 'West'}
 
-gazetteer = []
-gazetteer.append(start)
-
+gazetteer = [start]
 
 for instruction in data.split(', '):
     # find out which direction we're facing after turning
@@ -29,30 +27,36 @@ for instruction in data.split(', '):
     # move our location that many blocks in the correct direction, record each step
     for step in range(distance):
         if facing == 0:
-            cur_loc[1] = cur_loc[1] - 1
+            cur_loc[1] -= 1
         elif facing == 90:
-            cur_loc[0] = cur_loc[0] + 1
+            cur_loc[0] += 1
         elif facing == 180:
-            cur_loc[1] = cur_loc[1] + 1
+            cur_loc[1] += 1
         elif facing == 270:
-            cur_loc[0] = cur_loc[0] - 1
+            cur_loc[0] -= 1
 
         distance_from_origin = abs(cur_loc[0]) + abs(cur_loc[1])
 
         if cur_loc in gazetteer:
-            print("{}: We moved {}, {} blocks and are now at: {} a distance of {} blocks from {}".format(instruction,
-                                                                                                         directions[
-                                                                                                             facing],
-                                                                                                         distance,
-                                                                                                         cur_loc,
-                                                                                                         distance_from_origin,
-                                                                                                         start))
+            print("{}: We moved {} {} blocks and are now at: {}.".format(instruction,
+                                                                         directions[
+                                                                             facing],
+                                                                         distance,
+                                                                         cur_loc,
+                                                                         distance_from_origin,
+                                                                         start))
             print("We've been here before, we're {} blocks from {}.".format(distance_from_origin, start))
             sys.exit(0)
 
         gazetteer.append(cur_loc[:])
 
-    print("{}: We moved {}, {} blocks and are now at: {} a distance of {} blocks from {}".format(instruction, directions[facing], distance, cur_loc, distance_from_origin, start))
+    distance_from_origin = abs(cur_loc[0]) + abs(cur_loc[1])
+
+    print("{}: We moved {}, {} blocks and are now at: {} a distance of {} blocks from {}".format(instruction,
+                                                                                                 directions[facing],
+                                                                                                 distance, cur_loc,
+                                                                                                 distance_from_origin,
+                                                                                                 start))
 
 total_distance = abs(cur_loc[0]) + abs(cur_loc[1])
 print("Total distance from {} to {} is: {}".format(start, cur_loc, total_distance))
