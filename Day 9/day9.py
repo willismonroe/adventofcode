f6ed8b8cb000d4d780aea68d3d68680596c564b3
M. Willis Monroe <willismonroe@gmail.com>
2016-12-08 22:29:32 -0800

    minor progress

--- Day 9/day9.py
+++ Day 9/day9.py
@@ -2,12 +2,16 @@ import re
 
 REGEX = re.compile("\((\d+x\d+)\)")
 
-
+new_string = ''
 def decompress(string):
+    global new_string
     for m in REGEX.finditer(string):
         slice, times = map(int, m.group(1).split('x'))
         start, end = m.start(), m.end()
-        print(string[start:end+5])
+        slice = string[end:end+slice]
+        print("{} means repeat \"{}\" {} times".format(m.group(1), slice, times))
+        new_string += slice * times
+
 
 if __name__ == '__main__':
     with open('input.txt') as f:
