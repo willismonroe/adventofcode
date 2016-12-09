7164b6a064020ce16f319e6cc278949f95b78232
M. Willis Monroe <willismonroe@gmail.com>
2016-12-09 11:46:46 -0800

    Day9 finished with tests, needed help on part 2

--- Day 9/day9.py
+++ Day 9/day9.py
@@ -2,18 +2,56 @@ import re
 
 REGEX = re.compile("\((\d+x\d+)\)")
 
-new_string = ''
-def decompress(string):
-    global new_string
-    for m in REGEX.finditer(string):
-        slice, times = map(int, m.group(1).split('x'))
-        start, end = m.start(), m.end()
-        slice = string[end:end+slice]
-        print("{} means repeat \"{}\" {} times".format(m.group(1), slice, times))
-        new_string += slice * times
 
+def decompress(s):
+    m = re.search("\((\d+)x(\d+)\)", s)
+    if not m:
+        return len(s)
+    start = m.start(0)
+    slice_len = int(m.group(1))
+    times = int(m.group(2))
+    i = start + len(m.group())
+    return len(s[:start]) + len(s[i:i+slice_len]) * times + decompress(s[i+slice_len:])
+
+def decompress_v2(s):
+    m = re.search("\((\d+)x(\d+)\)", s)
+    if not m:
+        return len(s)
+    start = m.start(0)
+    slice_len = int(m.group(1))
+    times = int(m.group(2))
+    i = start + len(m.group())
+    return len(s[:start]) + decompress_v2(s[i:i+slice_len]) * times + decompress_v2(s[i+slice_len:])
+
+
+# Old method where I created the actual string
+# def decompress(string, new_string=''):
+#     print("Starting decompress with string: {}...".format(string[:40]))
+#     if len(new_string) > 0:
+#         print("Carried over new_string: {}...".format(new_string[:40]))
+#     # Check for a match
+#     m = REGEX.search(string)
+#     if not m:
+#         print("No matches in {}...".format(string[:40]))
+#         new_string += string
+#         print("**Final Output: {}.\n".format(new_string))
+#         return new_string
+#     # set up useful variables
+#     slice_len, times = map(int, m.group(1).split('x'))
+#     start, end = m.start(1)-1, m.end(1)+1
+#     slice = string[end:end + slice_len]
+#     # add anything before the first match
+#     new_string += string[:start]
+#     print("{} means repeat \"{}\" {} times".format(m.group(1), slice, times))
+#     # add repeated text
+#     new_string += slice * times
+#     print("new_string: {}.".format(new_string))
+#     # pass the rest to decompress again
+#     print("Passing {} to decompress again.".format(string[end + slice_len:]))
+#     return decompress(string[end + slice_len:], new_string)
 
 if __name__ == '__main__':
     with open('input.txt') as f:
         data = f.read()
-    print(len(decompress(data)))
+    print(decompress(data))
+    print(decompress_v2(data))
--- Day 9/tests.py
+++ Day 9/tests.py
@@ -6,6 +6,45 @@ class Day2Tests(unittest.TestCase):
     def test_True(self):
         self.assertTrue(True)
 
+    def test_Day9_Example(self):
+        string = 'ADVENT'
+        self.assertEqual(day9.decompress(string), 6)
+
+    def test_Day9_Example_2(self):
+        string = 'A(1x5)BC'
+        self.assertEqual(day9.decompress(string), 7)
+
+    def test_Day9_Example_3(self):
+        string = '(3x3)XYZ'
+        self.assertEqual(day9.decompress(string), 9)
+
+    def test_Day9_Example_4(self):
+        string = 'A(2x2)BCD(2x2)EFG'
+        self.assertEqual(day9.decompress(string), 11)
+
+    def test_Day9_Example_5(self):
+        string = 'A(2x2)BCD(2x2)EFG'
+        self.assertEqual(day9.decompress(string), 11)
+
+    def test_Day9_Example_6(self):
+        string = '(6x1)(1x3)A'
+        self.assertEqual(day9.decompress(string), 6)
+
+    def test_Day9_2Example(self):
+        string = '(3x3)XYZ'
+        self.assertEqual(day9.decompress_v2(string), 9)
+
+    def test_Day9_2Example_2(self):
+        string = 'X(8x2)(3x3)ABCY'
+        self.assertEqual(day9.decompress_v2(string), len('XABCABCABCABCABCABCY'))
+
+    def test_Day9_2Example_3(self):
+        string = '(27x12)(20x12)(13x14)(7x10)(1x12)A'
+        self.assertEqual(day9.decompress_v2(string), 241920)
+
+    def test_Day9_2Example_4(self):
+        string = '(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN'
+        self.assertEqual(day9.decompress_v2(string), 445)
 
 
 if __name__ == '__main__':
