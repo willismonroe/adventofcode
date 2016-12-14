fc21ab9ab5355ddf3f5a8d402a2b00174d8ae185
M. Willis Monroe <willismonroe@gmail.com>
2016-12-13 23:44:20 -0800

    Day14 optimizations, but part2 isn't working in the tests

--- Day 14/day14.py
+++ Day 14/day14.py
@@ -6,42 +6,34 @@ re_3 = re.compile(r'(.)\1{2}')
 re_5 = '({})\1{4}'
 
 @functools.lru_cache(maxsize=2048)
-def get_hash(string):
-    return md5(str.encode(string)).hexdigest()
-
-@functools.lru_cache(maxsize=2048)
-def stretch_hash(hash):
-    for n in range(2016):
-        hash = get_hash(hash)
+def get_hash(hash, stretch=False):
+    if stretch:
+        for n in range(2017):
+            hash = get_hash(hash)
+    else:
+        hash = md5(str.encode(hash)).hexdigest()
     return hash
 
 def solve(salt, stretch=False):
     keys = []
     i = 0
     while len(keys) < 64:
-        if stretch:
-            hash = stretch_hash(get_hash(salt + str(i)))
-        else:
-            hash = get_hash(salt + str(i))
+        hash = get_hash(salt + str(i), stretch)
         if re_3.search(hash):
             character = re_3.search(hash).group()[0]
             re_5 = re.compile(r'(' + character + r')\1{4}')
             tmp_int = i + 1
             while tmp_int - i < 1000:
-                if stretch:
-                    second_hash = stretch_hash(get_hash(salt + str(tmp_int)))
-                else:
-                    second_hash = get_hash(salt + str(tmp_int))
+                second_hash = get_hash(salt + str(tmp_int), stretch)
                 if re_5.search(second_hash):
                     keys.append((hash, i))
                     break
-
                 tmp_int += 1
         i += 1
     return i - 1
 
 if __name__ == '__main__':
     salt = 'ngcjuoqr'
-    #salt = 'abc'
+    salt = 'abc'
 
     print(solve(salt, True))
