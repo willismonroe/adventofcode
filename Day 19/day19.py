683cf1f91f8ac8d457f56a3ac698f9adb29b40bb
M. Willis Monroe <willismonroe@gmail.com>
2016-12-19 10:20:04 -0800

    Day19 part 2 solved with help

--- Day 19/day19.py
+++ Day 19/day19.py
@@ -1,4 +1,5 @@
 import math
+import collections
 
 def solve(data):
     ring = [1] * data
@@ -21,26 +22,24 @@ def solve(data):
     return (data - 2**math.floor(math.log(data, 2))) * 2 + 1
 
 def solve_v2(data):
-    ring = [1] * data
-    solved = 0
-    while not solved:
-        if ring.count(len(ring)) == 1:
-            solved = 1
-            break
-        for elf_num in range(len(ring)):
-            elf = ring[elf_num]
-            if elf == 0:
-                pass
-            else:
-                slice = round(elf_num + len(ring)/2)
-                #next_elf = next(i for i, v in enumerate(ring[elf_num + 1:] + ring[:elf_num]) if v > 0) + 1 + elf_num
-                next_elf = next(i for i, v in enumerate(ring[slice:] + ring[:slice - 1]) if v > 0) + slice
-                ring[elf_num] += ring[next_elf % len(ring)]
-                ring[next_elf % len(ring)] = 0
-    return ring
+    left = collections.deque()
+    right = collections.deque()
+    for i in range(1, data+1):
+        if i < (data // 2) + 1:
+            left.append(i)
+        else:
+            right.appendleft(i)
+    while left and right:
+        if len(left) > len(right):
+            left.pop()
+        else:
+            right.pop()
+        right.appendleft(left.popleft())
+        left.append(right.pop())
+    return left[0] or right[0]
 
 if __name__ == '__main__':
     data = 3005290
-    data = 5
+    data = 15
 
     print(solve_v2(data))
