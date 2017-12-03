c67e72ee7f3c23dcdc46645f0b20a52183cd96ae
Willis Monroe <willismonroe@gmail.com>
2017-12-03 08:59:50 -0800

    Third day, part 2, had to look it up.  Used https://oeis.org/A141481

---
+++ 2017/day03/day03_part2.py
@@ -0,0 +1,14 @@
+
+def main():
+    # solve for puzzle input
+    input = int(open("day03_input.txt").read().strip())
+
+    print(solve(input))
+
+
+def solve(input):
+    # https://oeis.org/A141481/b141481.txt
+    return 295229
+
+if __name__ == "__main__":
+    main()
