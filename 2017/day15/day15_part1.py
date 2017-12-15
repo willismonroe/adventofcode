a21fe4d76fac1e7fc2f08b5c1aa8fb64e5b1bfc4
Willis Monroe <willismonroe@gmail.com>
2017-12-14 22:16:26 -0800

    Day15 with tests

---
+++ 2017/day15/day15_part1.py
@@ -0,0 +1,44 @@
+import os
+
+
+def main():
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+    input = open("day15_input.txt").read().splitlines()
+
+    print(solve(input))
+
+
+class Generator:
+    def __init__(self, starting_value, factor, multiple=1):
+        self.previous_value = starting_value
+        self.factor = factor
+        self.multiple = multiple
+
+    def generate(self):
+        self.previous_value = (self.previous_value * self.factor) % 2147483647
+        if self.previous_value % self.multiple == 0:
+            return self.previous_value
+        else:
+            return self.generate()
+
+def to_binary(value):
+    # 0xFFFF = '0b1111111111111111' if we do a bitwise '&'
+    # that matches the first 16 bits of the value thus selecting
+    # them.
+    return value & 0xFFFF
+
+
+def solve(input):
+    gen_a = Generator(int(input[0].split()[-1]), 16807)
+    gen_b = Generator(int(input[1].split()[-1]), 48271)
+    matches = 0
+    for i in range(40000000):
+        if to_binary(gen_a.generate()) == to_binary(gen_b.generate()):
+            matches += 1
+
+    return matches
+
+
+if __name__ == '__main__':
+    main()
