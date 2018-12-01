f975108beede1b0da784c164dea0fcdb0b78a49b
Willis Monroe <willismonroe@gmail.com>
2018-11-30 21:59:30 -0800

    First day of 2018 with javascript

---
+++ 2018/.eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "node": true
+    },
+    "extends": [
+	"eslint:recommended",
+	"plugin:prettier/recommended"
+    ],
+    "parserOptions": {
+        "ecmaVersion": 2015
+    },
+    "rules": {
+	"no-console": "off"
+    }
+};
